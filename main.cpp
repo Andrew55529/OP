6b485d95d747100edc53d2f495a9bf73a9c7b64a
Andrew55529 <lashkov8@gmail.com>
2021-07-02 12:02:01 +0300

    remake

--- main.cpp
+++ main.cpp
@@ -3,17 +3,12 @@
 //Box 0 size 4    box 1   size 2 box 2 size 1
 class Box {
 public:
-	Box(int max = 0, int aver = 0, int min = 0) {
-		boxs[0] = max;
-		boxs[1] = aver;
-		boxs[2] = min;
-	}
+	Box(int max = 0, int aver = 0, int min = 0) { boxs[0] = max; boxs[1] = aver; boxs[2] = min;	}
 	bool add(int size);
 	bool remove(int size);
 	int getcount(int size);
-	int sum() {
-		return (boxs[0] * 4 + boxs[1] * 2 + boxs[2]);
-	}
+	void print();
+	int sum() { return (boxs[0] * 4 + boxs[1] * 2 + boxs[2]);}
 private:
 	int boxs[3];
 };
@@ -40,29 +35,45 @@ int Box::getcount(int size) {
 	else return -1;
 }
 
-
+void Box::print() {
+	for (int ii = 0; ii < boxs[0]; ii++) {
+		std::cout << "4___";
+	}
+	for (int ii = 0; ii < boxs[1]; ii++) {
+		std::cout << "2_";
+	}
+	for (int ii = 0; ii < boxs[2]; ii++) {
+		std::cout << "1";
+	}
+	std::cout << std::endl;
+}
 
 
 class Cont {
 public:
 	Cont(int m, int n, Box bo) : m(m), n(n) {
 		_bo = bo;
-		_boarr = new Box[3];
+		_boarr = new Box [3];
 	};
 	void placeall() {
 		for (int i = 0; i < n; ++i) {
 			place(i);
-			std::cout << std::endl;
 		}
 	}
-
+	void print() {
+		for (int i = n - 1; i >= 0; --i) {
+			_boarr[i].print();
+		}
+	}
 private:
 	Box _bo;
 	int m, n; //n-строчки
 	Box* _boarr;
 
 	void place(int rad) {
-		while (_boarr[rad].sum() < m) {
+		int itt=0;
+		while (_boarr[rad].sum() < m &&itt<m) {
+			++itt;
 			if (_boarr[rad].sum() <= m - 4 && _bo.getcount(0) > 0) { _boarr[rad].add(0); _bo.remove(0); }
 			else if (_boarr[rad].sum() <= m - 2 && _bo.getcount(1) > 0) {
 				_boarr[rad].add(1); _bo.remove(1);
@@ -70,14 +81,14 @@ private:
 			else if (_boarr[rad].sum() <= m - 1 && _bo.getcount(2) > 0) {
 				_boarr[rad].add(2); _bo.remove(2);
 			}
-			std::cout << _boarr[rad].sum();
 		}
 	}
-
 };
 
 int main() {
 	Box bo(1, 1, 10);
 	Cont container(5, 3, bo);
 	container.placeall();
+	container.print();
+
 }
